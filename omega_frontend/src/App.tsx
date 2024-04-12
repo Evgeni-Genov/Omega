80817a5b07ff85cb255f273ef1a91abf7a95a316
egenov <genov526@gmail.com>
2024-04-12 16:32:37 +0300

    refactor: configure and start FE

---
+++ omega_frontend/src/App.tsx
@@ -0,0 +1,15 @@
+// App.tsx
+
+import React from 'react';
+import Login from './assets/login/Login.tsx';
+
+const App: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to Omega</h1>
+            <Login/>
+        </div>
+    );
+};
+
+export default App;
